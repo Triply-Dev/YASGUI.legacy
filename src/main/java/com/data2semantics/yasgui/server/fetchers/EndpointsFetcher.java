c95c224009098fa2acdd9892b369680de6ab9e2b
Laurens Rietveld <laurens.rietveld@gmail.com>
2012-09-13 22:44:23 +0200

    added intial implementation of endpoints fetcher

---
+++ src/main/java/com/data2semantics/yasgui/server/fetchers/EndpointsFetcher.java
@@ -0,0 +1,18 @@
+package com.data2semantics.yasgui.server.fetchers;
+
+public class EndpointsFetcher {
+//	PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+//		PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+//		PREFIX dcat: <http://www.w3.org/ns/dcat#>
+//		PREFIX dcterms: <http://purl.org/dc/terms/>
+//
+//
+//		SELECT DISTINCT ?dataset ?datasetTitle ?datasetDescription ?endpoint  {
+//		  ?dataset dcat:distribution ?distribution.
+//		?distribution dcterms:format ?format.
+//		?format rdf:value 'api/sparql'.
+//		?distribution dcat:accessURL ?endpoint.
+//		?dataset dcterms:title ?datasetTitle;
+//			dcterms:description ?datasetDescription.
+//		} LIMIT 50
+}
