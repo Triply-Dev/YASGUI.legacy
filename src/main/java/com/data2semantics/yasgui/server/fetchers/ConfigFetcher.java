56afee3e131d812a2635e66889d66ebb7eba64f7
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-02-19 22:23:27 +0100

    removed unused config servlet and hostpage servlet implemented jsp index file

---
+++ src/main/java/com/data2semantics/yasgui/server/fetchers/ConfigFetcher.java
@@ -0,0 +1,34 @@
+package com.data2semantics.yasgui.server.fetchers;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import com.google.gwt.core.client.GWT;
+
+/**
+ * Servlet implementation class ConfigServlet
+ */
+public class ConfigFetcher  {
+	private static String CONFIG_FILE = "config/config.json";
+	public static String getJson() throws FileNotFoundException, IOException {
+		String jsonString;
+		File configFile = new File(CONFIG_FILE);
+		if (!configFile.exists()) {
+			throw new IOException("Unable to load config file from server. Trying to load: " + configFile.getAbsolutePath());
+		} else {
+			jsonString = IOUtils.toString(new FileReader(configFile));
+			try {
+				new JSONObject(jsonString);
+			} catch (Exception e) {
+				throw new IOException("Unable to parse config file on server", e);
+			}
+			
+		}
+		return jsonString;
+	}
+
+}
