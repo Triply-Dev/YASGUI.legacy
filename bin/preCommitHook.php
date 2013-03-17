5f057c972df50623580257ec72d0403c21f3c068
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-03-17 18:31:33 +0100

    initial implementation of commit hook

--- bin/preCommitHook.php
+++ bin/preCommitHook.php
@@ -1,1 +1,25 @@
+#!/usr/bin/php
+<?php
 
+//most important thing: is our config parsable, and are sensitive things such as api keys excluded?
+
+
+
+
+
+$exit_status = checkConfigFile();
+
+
+
+
+if ($exit_status) {
+	echo "Invalid commit, stopping now\n";
+}
+exit((int)$exit_status); //0: succes, 1, otherwise
+
+
+
+
+function checkConfigFile() {
+	return false;
+}
