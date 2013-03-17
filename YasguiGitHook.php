1235f0405981d89555bd73d79d098b366f983d62
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-03-17 19:51:11 +0100

    initial commit for deployment hoook

--- YasguiGitHook.php
+++ YasguiGitHook.php
@@ -1,3 +1,16 @@
 <?php
+$deploymentConfig = parse_init_file("hookConfig.ini", true);
+
+//add mail function faulure notification and stuff
+
+//parse json
+
+//if commit on dev
+////pull result
+////build
+////parse/replace/store config values
+////copy to tomcat
+
+//if commit on master
 
 
