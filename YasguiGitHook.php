9515e3e090a14311810392aa19112c6f8b894c55
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-03-17 21:54:37 +0100

    test commit

--- YasguiGitHook.php
+++ YasguiGitHook.php
@@ -1,5 +1,22 @@
 <?php
-$deploymentConfig = parse_init_file("hookConfig.ini", true);
+error_reporting(1);
+// $deploymentConfig = parse_init_file("hookConfig.ini", true);
+$payload = $_POST['payload'];
+
+$payload = file_get_contents("payload.json");
+
+if (strlen($payload)) {
+	$json = json_decode($payload, true);
+	if (!$json) mailError("Unable to decode json");
+	
+	var_export($json);
+} 
+
+
+
+function mailError($error) {
+	echo $error."\n";exit;
+}
 
 //add mail function faulure notification and stuff
 
