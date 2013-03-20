a7a32c58a8aa53b1ef93f1dec0f9421c876563d1
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-03-20 02:14:25 +0100

    added daemon functionality

--- GitHook.php
+++ GitHook.php
@@ -1,17 +1,21 @@
 <?php
+
+$startTime = time();
 chdir("/home/lrd900/gitCode/yasgui_deploy");
 include 'Helper.php';
 error_reporting(1);
+$config = Helper::getConfig();
 
-$i = 0;
 $payload = $_POST['payload'];
+
+
 
 if (strlen($payload)) {
 	$json = json_decode($payload, true);
 	if (!$json) Helper::mailError(__FILE__, __LINE__, "Unable to decode json");
 	
 	if ($json['ref'] === "refs/heads/master") {
-		shell_exec("./CompileAndDeploy.php master");
+		executeIfReady("./CompileAndDeploy.php master");
 	} else if ($json['ref'] === "refs/heads/dev") {
 		
 	} else if ($json[ref] === "refs/heads/deployment-git-hook") {
@@ -21,6 +25,25 @@ if (strlen($payload)) {
 		}
 	}
 } 
+
+function executeIfReady($command) {
+	global $startTime, $config;
+	
+	if ((time() - $startTime) > ($config['shell']['maxTimeout'] * 60)) {
+		Helper::mailError(__FILE__, __LINE__, "We waited too long for other processes to finish. Just stop trying to deploy. Number of processes still running: ".getNumProcesses($command));
+		exit;
+	}
+	$numProcs = getNumProcesses($command);
+	if ($numProcs > 0) {
+		sleep(10);
+		executeIfReady($command);
+	} else {
+		shell_exec($command ." &");
+	}
+}
+function getNumProcesses($cmd) {
+	return shell_exec("ps aux | grep '[s]h -c ".$cmd."' | wc -l");
+}
 
 
 
