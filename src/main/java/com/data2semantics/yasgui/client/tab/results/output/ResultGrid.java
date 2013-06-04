59548544a6c70ea55abaa9e7a76b0f72e0d429a2
Laurens Rietveld <laurens.rietveld@gmail.com>
2013-06-04 21:00:18 +0200

    #65 #124 Wrap results in listgrid fixed grouping functionality

--- src/main/java/com/data2semantics/yasgui/client/tab/results/output/ResultGrid.java
+++ src/main/java/com/data2semantics/yasgui/client/tab/results/output/ResultGrid.java
@@ -28,17 +28,17 @@ package com.data2semantics.yasgui.client.tab.results.output;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map.Entry;
+
 import com.data2semantics.yasgui.client.View;
 import com.data2semantics.yasgui.client.helpers.Helper;
 import com.data2semantics.yasgui.client.tab.results.input.ResultsHelper;
 import com.data2semantics.yasgui.client.tab.results.input.SparqlResults;
 import com.data2semantics.yasgui.shared.Prefix;
 import com.smartgwt.client.types.Alignment;
-import com.smartgwt.client.types.Overflow;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.HTMLPane;
-import com.smartgwt.client.widgets.Label;
+import com.smartgwt.client.widgets.grid.CellFormatter;
 import com.smartgwt.client.widgets.grid.ListGrid;
 import com.smartgwt.client.widgets.grid.ListGridField;
 import com.smartgwt.client.widgets.grid.ListGridRecord;
@@ -71,49 +71,8 @@ public class ResultGrid extends ListGrid {
 	 * @param jsonString
 	 */
 	public void drawQueryResults() {
-		List<ListGridField> listGridFields = getVarsAsListGridFields(sparqlResults.getVariables());
-		setFields(listGridFields.toArray(new ListGridField[listGridFields.size()]));
-		List<ListGridRecord> rows = getSolutionsAsGridRecords(sparqlResults.getBindings());
-		setData(rows.toArray(new ListGridRecord[rows.size()]));
-	}
-	
-	/**
-	 * Overridden method of ListGrid. Need to use this, because otherwise we cannot add different widgets to different cells
-	 */
-	protected Canvas createRecordComponent(ListGridRecord row, Integer colNum) {
-		// fieldname is the identifier of the column, in our case the same as
-		// the column header
-		String colName = this.getFieldName(colNum);
-		
-		//the numbering field created by smartgwt has field name starting with $
-		if (!colName.startsWith("$")) { 
-			HashMap<String, HashMap<String, String>> bindings = solutions.get(row.getAttributeAsInt(SOLUTION_ATTRIBUTE));
-			HashMap<String, String> binding = bindings.get(colName);
-			if (binding != null) {
-				String type = binding.get("type");
-				if (type.equals("uri")) {
-					final String uri = binding.get("value");
-					return Helper.getLinkNewWindow(ResultsHelper.getShortUri(uri, queryPrefixes), uri);
-				} else if (type.equals("literal") || binding.get("type").equals("typed-literal")) {
-					String literal = ResultsHelper.getLiteralFromBinding(binding);
-					Label label = new Label(literal);
-					label.setOverflow(Overflow.VISIBLE);
-					label.setWidth100();
-					label.setAutoHeight();
-					label.setCanSelectText(true);
-					return label;
-				} else {
-					//is bnode
-					String uri = binding.get("value");
-					Label label = new Label(uri);
-					label.setHeight100();
-					label.setCanSelectText(true);
-					label.setWidth100();
-					return label;
-				}
-			}
-		}
-		return null;
+		setVarsAsListGridFields(sparqlResults.getVariables());
+		setSolutionsAsGridRecords(sparqlResults.getBindings());
 	}
 	
 	/**
@@ -122,16 +81,19 @@ public class ResultGrid extends ListGrid {
 	 * @param solutions
 	 * @return
 	 */
-	private ArrayList<ListGridRecord> getSolutionsAsGridRecords(ArrayList<HashMap<String, HashMap<String, String>>> solutions) {
+	private void setSolutionsAsGridRecords(ArrayList<HashMap<String, HashMap<String, String>>> solutions) {
 		ArrayList<ListGridRecord> rows = new ArrayList<ListGridRecord>();
 		for (HashMap<String, HashMap<String, String>> solution: solutions) {
 			this.solutions.put(solution.hashCode(), solution);
 			ListGridRecord row = new ListGridRecord();
 			row.setAttribute(SOLUTION_ATTRIBUTE, solution.hashCode());
+			for (Entry<String, HashMap<String, String>> entry : solution.entrySet()) {
+			    String variable = entry.getKey();
+			    row.setAttribute(variable, entry.getValue().get("value"));
+			}
 			rows.add(row);
-			
 		}
-		return rows;
+		setData(rows.toArray(new ListGridRecord[rows.size()]));
 	}
 	
 	/**
@@ -139,9 +101,9 @@ public class ResultGrid extends ListGrid {
 	 * @param vars
 	 * @return
 	 */
-	private ArrayList<ListGridField> getVarsAsListGridFields(ArrayList<String> vars) {
+	private void setVarsAsListGridFields(ArrayList<String> vars) {
 		ArrayList<ListGridField> listGridFields = new ArrayList<ListGridField>();
-		for(String var: vars){
+		for(final String var: vars){
 			ListGridField field = new ListGridField(var, var);
 			field.setSortNormalizer(new SortNormalizer(){
 				public Object normalize(ListGridRecord record, String fieldName) {
@@ -149,12 +111,33 @@ public class ResultGrid extends ListGrid {
 					HashMap<String, String> binding = bindings.get(fieldName);
 					return binding.get("value");
 				}});
+			field.setCellFormatter(new CellFormatter(){
+				@Override
+				public String format(Object value, ListGridRecord record, int rowNum, int colNum) {
+					view.getLogger().severe("value: " + value.toString());
+					HashMap<String, HashMap<String, String>> bindings = solutions.get(record.getAttributeAsInt(SOLUTION_ATTRIBUTE));
+					HashMap<String, String> binding = bindings.get(var);
+					String type = binding.get("type");
+					if (type.equals("uri")) {
+						final String uri = binding.get("value");
+						return new Canvas().linkHTML(ResultsHelper.getShortUri(uri, queryPrefixes), uri, "_blank", null, null, null);
+					} else {
+						String labelText;
+						if (type.equals("literal") || binding.get("type").equals("typed-literal")) {
+							labelText = ResultsHelper.getLiteralFromBinding(binding);
+						} else {
+							labelText = binding.get("value");
+						}
+						return labelText;
+					}
+					
+				}});
 			field.setCellAlign(Alignment.LEFT);
 			field.setAlign(Alignment.CENTER); //for header
 			
 			listGridFields.add(field);
 		}
-		return listGridFields;
+		setFields(listGridFields.toArray(new ListGridField[listGridFields.size()]));
 	}
 	
 	
