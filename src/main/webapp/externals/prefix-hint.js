21bf0c82dc37b912f1848ef9e00c9e0c961c5f1c
Laurens Rietveld <laurens.rietveld@gmail.com>
2013-06-13 23:21:43 +0200

    #126 improve auto-adding of prefixes

--- src/main/webapp/externals/prefix-hint.js
+++ src/main/webapp/externals/prefix-hint.js
@@ -88,3 +88,139 @@
 		return found;
 	}
 })();
+
+
+function getNextNonWsToken(cm, lineNumber, charNumber) {
+	if (charNumber == undefined) charNumber = 1;
+	var token = cm.getTokenAt({line : lineNumber,ch : charNumber});
+	if (token == null || token == undefined || token.end < charNumber) {
+		return null;
+	}
+	if (token.className == "sp-ws") {
+		return getNextNonWsToken(cm, lineNumber, token.end+1);
+	}
+	return token;
+}
+
+/**
+ * Check whether typed prefix is declared. If not, automatically add declaration using list from prefix.cc
+ * @param cm
+ */
+function appendPrefixIfNeeded(cm) {
+	var cur = cm.getCursor();
+	var token = cm.getTokenAt(cur);
+	if (token.className == "sp-prefixed") {
+//		if (token.string.endsWith(":")) {
+		var colonIndex = token.string.indexOf(":");
+		if (colonIndex !== -1) {
+			//check first token isnt PREFIX, and previous token isnt a '<' (i.e. we are in a uri)
+			var firstTokenString = getNextNonWsToken(cm, cur.line).string.toUpperCase();
+			var previousToken = cm.getTokenAt({line: cur.line, ch: token.start});//needs to be null (beginning of line), or whitespace
+			if (firstTokenString != "PREFIX" && (previousToken.className == "sp-ws" || previousToken.className == null)) {
+				//check whether it isnt defined already (saves us from looping through the array)
+				var currentPrefix = token.string.substring(0, colonIndex+1);
+				var queryPrefixes = getPrefixesFromQuery(cm);
+				if (queryPrefixes.length == 0 || $.inArray(currentPrefix, queryPrefixes) == -1) {
+					for (var i = 0; i < prefixes.length; i++) {
+						var prefix = prefixes[i].substring(0, currentPrefix.length);
+						if (prefix == currentPrefix) {
+							appendToPrefixes(cm, prefixes[i]);
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
+/**
+ * Get defined prefixes from query as array, in format ["rdf:", "rdfs:"]
+ * @param cm
+ * @returns {Array}
+ */
+function getPrefixesFromQuery(cm) {
+	var queryPrefixes = [];
+	var numLines = cm.lineCount();
+	for (var i = 0; i < numLines; i++) {
+		var firstToken = getNextNonWsToken(cm, i);
+		if (firstToken != null && firstToken.string == "PREFIX") {
+			var prefix = getNextNonWsToken(cm, i, firstToken.end + 1);
+			if (prefix.string != ":") {
+				queryPrefixes.push(prefix.string);
+			}
+		}
+	}
+	return queryPrefixes;
+}
+
+/**
+ * Append prefix declaration to list of prefixes in query window.
+ * @param cm
+ * @param prefix
+ */
+function appendToPrefixes(cm, prefix) {
+	var lastPrefix = null;
+	var lastPrefixLine = 0;
+	var numLines = cm.lineCount();
+	for (var i = 0; i < numLines; i++) {
+		var firstToken = getNextNonWsToken(cm, i);
+		if (firstToken != null && (firstToken.string == "PREFIX" || firstToken.string == "BASE")) {
+			lastPrefix = firstToken;
+			lastPrefixLine = i;
+		}
+	}
+
+	if (lastPrefix == null) {
+		cm.replaceRange("PREFIX " + prefix + "\n", {line: 0, ch:0});
+	} else {
+		var previousIndent = getIndentFromLine(cm, lastPrefixLine);
+		cm.replaceRange("\n" + previousIndent + "PREFIX " + prefix, {line: lastPrefixLine});
+	}
+	
+}
+
+/**
+ * Get the used indentation for a certain line
+ * @param cm
+ * @param line
+ * @param charNumber
+ * @returns
+ */
+function getIndentFromLine(cm, line, charNumber) {
+	if (charNumber == undefined) charNumber = 1;
+	var token = cm.getTokenAt({line : line,ch : charNumber});
+	if (token == null || token == undefined || token.className != "sp-ws") {
+		return "";
+	} else {
+		return token.string + getIndentFromLine(cm, line, token.end+1);
+	}
+}
+
+
+function updateBookmarkCmHeight(queryInputId) {
+	cmHeight = sparqlHighlight[queryInputId].getWrapperElement().offsetHeight;
+	if (sparqlHighlightHeight[queryInputId]) {
+		if (cmHeight != sparqlHighlightHeight[queryInputId]) {
+			sparqlHighlightHeight[queryInputId] = cmHeight;
+			adjustBookmarkQueryInputForContent(cmHeight);
+		}
+	} else {
+		sparqlHighlightHeight[queryInputId] = cmHeight;
+	}
+}
+
+
+if (typeof String.prototype.startsWith != 'function') {
+	  String.prototype.startsWith = function (str){
+	    return this.slice(0, str.length) == str;
+	  };
+}
+if (typeof String.prototype.endsWith != 'function') {
+	  String.prototype.endsWith = function (str){
+	    return this.slice(-str.length) == str;
+	  };
+}
+
+
