0d46f3e2c799f7c95996f35d83a1fc792725446e
Laurens Rietveld <laurens.rietveld@gmail.com>
2013-10-07 23:11:01 +0200

    updated changelog notification (mention bug report functionality)

--- src/main/java/com/data2semantics/yasgui/client/helpers/ChangelogHelper.java
+++ src/main/java/com/data2semantics/yasgui/client/helpers/ChangelogHelper.java
@@ -45,10 +45,9 @@ public class ChangelogHelper {
 				"<li>YASGUI now works offline as well (for browsers supporting HTML5)</li>\n" + 
 				"<li>Version change notifications</li>\n" + 
 				"<li>Local installation/integration of YASGUI is now more flexible/easy</li>\n" + 
-				"<li>Moved YASGUI to a more stable version</li>\n" + 
 			"</ul>";
 	private static String AUTO_FORMAT_SHORTCUT_NOTIFICATION = "Autoformat (part of) your query! Use 'CTRL-SHIFT-F'";
-	private static String RESET_SETTINGS_NOTIFICATION = "Now possible to reset your YASGUI settings";
+	private static String BUG_REPORT_NOTIFICATION = "Submit any bugs you encounter or features you desire!";
 	private static String SPECIFY_NAMED_GRAPH_NOTIFICATION = "Specify named or default graphs for SPARQL http requests";
 	private View view;
 	
@@ -89,7 +88,7 @@ public class ChangelogHelper {
 	private void showResetSettingsNotification() {
 		TooltipProperties tProp = new TooltipProperties();
 		tProp.setId(view.getElements().getConfigMenu().getDOM().getId());
-		tProp.setContent(RESET_SETTINGS_NOTIFICATION);
+		tProp.setContent(BUG_REPORT_NOTIFICATION);
 		tProp.setMy(TooltipProperties.POS_RIGHT_CENTER);
 		tProp.setAt(TooltipProperties.POS_LEFT_CENTER);
 		Helper.drawTooltip(tProp);
