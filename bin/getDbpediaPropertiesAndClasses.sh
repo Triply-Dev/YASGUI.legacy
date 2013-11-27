910588f9dcb11eeee5dd57fda72beba8413cce83
Laurens Rietveld <laurens.rietveld@gmail.com>
2013-11-27 21:04:03 +0100

    #141 improved fetching of dbpedia info, by inlcuding all link data

--- bin/getDbpediaPropertiesAndClasses.sh
+++ bin/getDbpediaPropertiesAndClasses.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#download all dbpedia ttl files (use ttl over nt because of proper encoding of ttl. Dbpedia ttl files are essential ntriple files with comments)
-echo "downloading ttl files"
+#download all dbpedia ttl files (prefer ttl over nt because of proper encoding of ttl. Dbpedia ttl files are essential ntriple files with comments)
+echo "downloading ttl/nt files"
 wget -r --no-parent --no-directories http://downloads.dbpedia.org/3.9/en/ --accept "*ttl.bz2"
+wget -r --no-parent --no-directories http://downloads.dbpedia.org/3.9/links/ --accept "*nt.bz2"
 
 
 #unzip them
@@ -10,15 +11,15 @@ bunzip2 *.bz2
 
 rm -f predicates.txt
 #get the predicate from the dump (in a very naive way). 
-echo "retrieving predicates from ttl files (naively)"
+echo "retrieving predicates from files"
 ls *.ttl | xargs awk '{print $2}' | sed '/^[^<]/ d' | sed '/[^>]$/ d' | sed 's/^<\(.*\)>$/\1/' >> predicates.txt
 
 rm -f classes.txt
-echo "retrieving classes from ttl files (naively)"
+echo "retrieving classes from files"
 ls *.ttl | xargs awk '{if ($2 == "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>") print $3}' | sed '/^[^<]/ d' | sed '/[^>]$/ d' | sed 's/^<\(.*\)>$/\1/' >> classes.txt
 
 #remove ttl files
-echo "removing old ttl files"
+echo "removing old downloaded files"
 rm *.ttl 
 
 echo "making predicate list unique"
