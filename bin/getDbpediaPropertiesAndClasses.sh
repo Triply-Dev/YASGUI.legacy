2d052be221b9be40c0ad362cb7e7f780bd3862ea
Laurens Rietveld <laurens.rietveld@gmail.com>
2013-11-29 13:50:22 +0100

    #141 added dbpedia ontology to the list of which we retrieve classes and properties

--- bin/getDbpediaPropertiesAndClasses.sh
+++ bin/getDbpediaPropertiesAndClasses.sh
@@ -3,11 +3,15 @@
 echo "downloading ttl/nt files"
 wget -r --no-parent --no-directories http://downloads.dbpedia.org/3.9/en/ --accept "*ttl.bz2"
 wget -r --no-parent --no-directories http://downloads.dbpedia.org/3.9/links/ --accept "*nt.bz2"
-
+wget http://downloads.dbpedia.org/3.9/dbpedia_3.9.owl.bz2
 
 #unzip them
 echo "unzipping files"
 bunzip2 *.bz2
+
+echo "first converting owl file to ntriple using rapper"
+rm -f dbpedia.nt
+rapper *.owl >> dbpedia.nt
 
 rm -f predicates.txt
 #get the predicate from the dump (in a very naive way). 
@@ -16,11 +20,13 @@ ls *.{nt,ttl} | xargs awk '{print $2}' | sed '/^[^<]/ d' | sed '/[^>]$/ d' | sed
 
 rm -f classes.txt
 echo "retrieving classes from files"
-ls *.{nt,ttl} | xargs awk '{if ($2 == "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>") print $3}' | sed '/^[^<]/ d' | sed '/[^>]$/ d' | sed 's/^<\(.*\)>$/\1/' >> classes.txt
+ls *.{nt,ttl} | xargs awk '{if ($2 == "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>" || $2 == "<http://www.w3.org/2000/01/rdf-schema#domain>" || $2 == "<http://www.w3.org/2000/01/rdf-schema#range>") print $3; else if ($2 == "<http://www.w3.org/2000/01/rdf-schema#subClassOf>") {print $1; print $3;}}' | sed '/^[^<]/ d' | sed '/[^>]$/ d' | sed 's/^<\(.*\)>$/\1/' >> classes.txt
+
 
 #remove ttl files
 echo "removing old downloaded files"
 rm *.ttl 
+rm *.nt
 
 echo "making predicate list unique"
 sort -u predicates.txt -o predicates.txt
