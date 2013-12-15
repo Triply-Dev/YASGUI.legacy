e2e219b20285bea2858eae86ad0ba484946bd5de
Laurens Rietveld <laurens.rietveld@gmail.com>
2013-12-15 10:18:09 +0100

    #204 refactored previous delta's, to deal with localhost endpoints

--- src/main/webapp/config/delta_1.sql
+++ src/main/webapp/config/delta_1.sql
@@ -1,21 +1,78 @@
-CREATE TABLE IF NOT EXISTS `DisabledPropertyEndpoints` (
-  `Endpoint` varchar(1000) NOT NULL,
-  `Method` enum('lazy','property') NOT NULL
+
+--
+-- Table structure for table `AccessibleEndpoints`
+--
+
+CREATE TABLE IF NOT EXISTS `AccessibleEndpoints` (
+  `EndpointId` int(11) NOT NULL,
+  `Accessible` tinyint(1) NOT NULL,
+  `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `LogPropertyFetcher` (
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Classes`
+--
+
+CREATE TABLE IF NOT EXISTS `Classes` (
+  `Uri` varchar(1000) NOT NULL,
+  `EndpointId` int(11) NOT NULL,
+  `Method` enum('query','queryResults') NOT NULL,
+  `Added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `CompletionEndpoints`
+--
+
+CREATE TABLE IF NOT EXISTS `CompletionEndpoints` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Endpoint` varchar(1000) NOT NULL,
+  `UserId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `CompletionsLog`
+--
+
+CREATE TABLE IF NOT EXISTS `CompletionsLog` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `EndpointId` int(11) NOT NULL,
+  `Type` enum('class','property') NOT NULL,
   `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Status` enum('successful','failed','fetching') NOT NULL,
   `Pagination` tinyint(1) NOT NULL DEFAULT '0',
   `Message` text,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=34 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `DisabledCompletionEndpoints`
+--
+
+CREATE TABLE IF NOT EXISTS `DisabledCompletionEndpoints` (
+  `EndpointId` int(11) NOT NULL,
+  `Type` enum('class','property') NOT NULL,
+  `Method` enum('query','queryResults') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Properties`
+--
 
 CREATE TABLE IF NOT EXISTS `Properties` (
   `Uri` varchar(1000) NOT NULL,
-  `Endpoint` varchar(1000) NOT NULL,
-  `Method` enum('lazy','property') NOT NULL,
+  `EndpointId` int(11) NOT NULL,
+  `Method` enum('query','queryResults') NOT NULL,
   `Added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
